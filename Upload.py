a1ac4f59dd4ff1b91a3dcd9ce3fb206caada7dbf
HeiTang <tim881205@gmail.com>
2020-04-01 17:22:13 +0800

    Fix UX

--- Upload.py
+++ Upload.py
@@ -16,7 +16,6 @@ SCOPES = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/a
 def delete_drive_service_file(service, file_id):
     service.files().delete(fileId=file_id).execute()
 
-
 def update_file(service, update_drive_service_name, local_file_path, update_drive_service_folder_id):
     """
     將本地端的檔案傳到雲端上
@@ -38,15 +37,13 @@ def update_file(service, update_drive_service_name, local_file_path, update_driv
     start = time.time()
     file_id = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
     end = time.time()
-    # print("上傳檔案成功！")
-    print('%10sλ 雲端檔案名稱： ' % " " + str(file_metadata['name']))
-    print('%10sλ 雲端檔案 ID： ' % " " + str(file_id['id']))
-    print('%10sλ 檔案大小： ' % " " + str(file_metadata_size) + ' byte')
-    print("%10sλ 上傳時間： " % " " + str(end-start))
-    print("\n")
+
+    print('%10s\033[0;34mλ 雲端檔案名稱：\033[0m' % " " + str(file_metadata['name']))
+    print('%10s\033[0;34mλ 雲端檔案 ID：\033[0m' % " " + str(file_id['id']))
+    print('%10s\033[0;34mλ 檔案大小：\033[0m' % " " + str(file_metadata_size) + ' byte')
+    print("%10s\033[0;34mλ 上傳時間：\033[0m" % " " + str(end-start) + '\n')
 
     return file_metadata['name'], file_id['id']
-
 
 def search_folder(service, update_drive_folder_name=None):
     """
@@ -89,40 +86,19 @@ def search_file(service, update_drive_service_name, is_delete_search_file=False)
     if not items:
         print('%10s雲端中未發現「' % " " + update_drive_service_name + '」檔案.' )
     else:
-        print('搜尋的檔案: ')
+        # print('搜尋的檔案: ')
         for item in items:
             times = 1
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+            # print(u'{0} ({1})'.format(item['name'], item['id']))
+            print('%10s雲端中發現「' % " " + u'{0} ({1})'.format(item['name'], item['id']) + '」檔案.' , end = ' ')
             if is_delete_search_file is True:
-                print("刪除檔案為:" + u'{0} ({1})'.format(item['name'], item['id']))
+                print("\033[0;31m進行刪除\033[0m")
                 delete_drive_service_file(service, file_id=item['id'])
 
             if times == len(items):
                 return item['id']
             else:
                 times += 1
-
-
-def trashed_file(service, is_delete_trashed_file=False):
-    """
-    抓取到雲端上垃圾桶內的全部檔案，進行刪除
-    :param service: 認證用
-    :param is_delete_trashed_file: 是否要刪除垃圾桶資料
-    :return:
-    """
-    results = service.files().list(fields="nextPageToken, files(id, name)", spaces='drive', q="trashed = true",
-                                   ).execute()
-    items = results.get('files', [])
-    if not items:
-        print('垃圾桶無任何資料.')
-    else:
-        print('垃圾桶檔案: ')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
-            if is_delete_trashed_file is True:
-                print("刪除檔案為:" + u'{0} ({1})'.format(item['name'], item['id']))
-                delete_drive_service_file(service, file_id=item['id'])
-
 
 def get_update_files_path_list(update_files_path):
     """
@@ -150,45 +126,40 @@ def main(is_update_file_function=False, update_drive_service_folder_name=None, u
     :param update_file_path: 要上傳檔案的位置以及名稱
     """
 
-    # print("is_update_file_function: %s" % is_update_file_function)
-    # print("update_drive_service_folder_name: %s" % update_drive_service_folder_name)
-
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
         flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('drive', 'v3', http=creds.authorize(Http()))
-    # print('*' * 10)
 
     if is_update_file_function is True:
-
+        
         if update_drive_service_name is None:  # 上傳資料夾內的所有檔案會跑這裡
-            # print("上傳資料夾內，所有檔案")
             UploadFilesName, UploadFilesPath = get_update_files_path_list(update_files_path = update_file_path)
             print("\n%8s比對重複檔案: " % " ")
             get_folder_id = search_folder(service = service, update_drive_folder_name = update_drive_service_folder_name)
-            # print(UploadFilesPath)
+            
+            #  搜尋上傳的檔案名稱是否有在雲端上並且刪除
             for UploadFileName in UploadFilesName:
-            #  搜尋要上傳的檔案名稱是否有在雲端上並且刪除
                 search_file(service = service, update_drive_service_name = UploadFileName, is_delete_search_file = True)
-            # # 檔案上傳到雲端上
-            print("\n%8s上傳檔案: " % " ")
-            for i in range(len(UploadFilesPath)):
+            
+            # 檔案上傳到雲端上
+            print("\n%8s\033[0;30;42m[上傳階段]\033[0m" % " ")
+            for i in range(len(UploadFilesPath)): 
                 update_file(service=service, update_drive_service_name=UploadFilesName[i],
                             local_file_path=UploadFilesPath[i], update_drive_service_folder_id=get_folder_id)
-            print("=====上傳檔案完成=====")
+            # print("%8s=====上傳檔案完成=====" % " ")
+            print("%8s\033[0;31;42m[  完成  ]\033[0m" % " ")
 
         else:  # 單純上傳一個檔案會跑這裡
             print(update_file_path + update_drive_service_name)
-            print("%8s比對重複檔案: " % " ")
-            # 清空 雲端垃圾桶檔案
-            # trashed_file(service=service, is_delete_trashed_file=True)
+            print("\n%8s比對重複檔案: " % " ")
+            
             get_folder_id = search_folder(service = service, update_drive_folder_name = update_drive_service_folder_name)
             # 搜尋要上傳的檔案名稱是否有在雲端上並且刪除
             search_file(service=service, update_drive_service_name=update_drive_service_name, is_delete_search_file=True)
             # 檔案上傳到雲端上
             update_file(service=service, update_drive_service_name=update_drive_service_name,
                         local_file_path=update_file_path + update_drive_service_name, update_drive_service_folder_id=get_folder_id)
-            print("=====上傳檔案完成=====")
-
+            print("%8s\033[0;31;42m[  完成  ]\033[0m" % " ")
